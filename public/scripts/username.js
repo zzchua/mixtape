2af524b8733285e99c0e8490dd0551e2a25a6244
Zack Chua <zzchua@cs.washington.edu>
2016-04-17 06:42:04 -0700

    removed unique user validation

--- public/scripts/username.js
+++ public/scripts/username.js
@@ -7,32 +7,22 @@ define(function(require) {
         var regex = /^[a-zA-Z0-9-_]{5,12}$/;
         if (handle.search(regex) == -1) {
             alert("handle needs to be between 5 and 11 characters");
-            window.location = "username.html";
         } else {
-            ref.child("users").on("value", function(snapshot) {
-                snapshot.forEach(function(data) {
-                    if (handle.toLowerCase() === data.handle.toLowerCase()) {
-                        alert("username has been taken");
-                        window.location = "username.html";
-                    }
-                });
-            });
-        }
         var uid = common.getCookies().uid;
-        ref.child("users").child(uid).update(
-            {
-                "handle": handle
-            },
-            function(error) {
-                if (!error) {
-                    document.cookie = "handle="+handle
-                    window.location = "feed.html";
-                } else {
-                    alert(error);
-                    window.location = "index.html";
+            ref.child("users").child(uid).update(
+                {
+                    "handle": handle
+                },
+                function(error) {
+                    if (!error) {
+                        document.cookie = "handle="+handle
+                        window.location = "feed.html";
+                    } else {
+                        alert(error);
+                        window.location = "index.html";
+                    }
                 }
-            }
-        );
-
+            );
+        }
     });
 });
