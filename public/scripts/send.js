df95d1cb34d8b5432eaa69822c3725fa60c4e045
kyaw <kyaw@uw.edu>
2016-04-17 01:16:55 -0700

    finish basic send

--- public/scripts/send.js
+++ public/scripts/send.js
@@ -1,9 +1,25 @@
 define(function(require) {
 	var common = require("./common");
 	var mix = require("./mix");
+	var $ = require("./jquery")
+	var ref = common.ref;
+	var reqsRef = ref.child('requests');
+    var recipients = [];
+	$(".fa-check-square").click(function() {
+    	var userId = $(this).parent().parent().attr('id');
+    	recipients = common.updateRecipients(recipients, userId, $(this));
+    });
+    
+
     document.getElementById("sendMixButton").onclick = function() {
-		mix.sendMix();
+		for(var i = 0; i < recipients.length; i++){
+			console.log(recipients[i]);
+			mix.sendMix(recipients[i]);
+		}
+		window.location = "feed.html";
 	};
+
+
 });
 
 
