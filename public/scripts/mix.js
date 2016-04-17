c627bef304b12e11b7a659c8b0f016e44947ca61
kyaw <kyaw@uw.edu>
2016-04-16 23:39:32 -0700

    improviding sendMix()

--- public/scripts/mix.js
+++ public/scripts/mix.js
@@ -3,14 +3,14 @@ define(function(require) {
     var ref = common.ref;
     document.getElementById("sendMixButton").onclick = sendMix;
 
-    function sendMix(){
-    	var reciever = "facebook:10156785213465257";
-    	var sender = "facebook:10156785213465257";
+    function sendMix(receiverId){
+    	var receiver = receiverId;
+    	var sender = common.getCookies().uid;
     	var songAlbum = document.getElementById("songAlbumInput").value;
     	var artist = document.getElementById("artistInput").value;
     	alert("sending mix " + artist + " " + songAlbum);
     	var mixesRef = ref.child("mixes");
-    	var mix = {"recievers":{"reciever":reciever},"sender":sender, 
+    	var mix = {"recievers":{"receiver":receiver},"sender":sender, 
     		"songAlbum":songAlbum, "artist":artist, "comments":{"comment":""}};
     	mixesRef.push(mix);
     	window.location = "feed.html";
