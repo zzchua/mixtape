fffe559ac7c47107f3d117fbdf120b96d157a0bc
Zack Chua <zzchua@cs.washington.edu>
2016-04-16 23:14:53 -0700

    added getCookies(), login/logout

--- public/scripts/common.js
+++ public/scripts/common.js
@@ -1,13 +1,40 @@
 define(function() {
 	var ref = new Firebase("https://scorching-heat-6803.firebaseio.com");
-		var common = {
-				ref: ref,
-			logout: (function logout() {
-						console.log("loggingout");
-						ref.unauth();
-						alert("You are logged out!! going back into the login screen");
-						window.location = "index.html";
-				})
+
+	// if no cookies set and logged in, set cookie
+	if (!document.cookie) {
+		var authData = ref.getAuth();
+		if (authData) {
+			document.cookie = "uid="+authData.uid;
+		}
+	}
+	var common = {
+			ref: ref,
+		logout: (function logout() {
+					console.log("loggingout");
+					ref.unauth();
+					alert("You are logged out!! going back into the login screen");
+					// delete cookies on logout:
+					document.cookie = "uid=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+					window.location = "index.html";
+			}),
+		getCookies: (function () {
+					var cookieObject = {};
+					var cookieArray = document.cookie.split(";");
+					for (var i = 0; i < cookieArray.length; i++) {
+						var keyVal = cookieArray[i].split("=");
+						if (keyVal) {
+							cookieObject[keyVal[0]] = keyVal[1];
+						}
+					}
+					return cookieObject;
+					/*
+					{
+						uid: "123",
+						other_cookie: cookie_val
+					}
+					*/
+		})
 	}
 	return common;
 });
--- public/scripts/feed.js
+++ public/scripts/feed.js
@@ -1,6 +1,7 @@
 define(function(require) {
-	var common = require("./common");
+    var common = require("./common");
     document.getElementById("special").onclick = common.logout;
+    alert(common.getCookies().uid);
 });
 
 
