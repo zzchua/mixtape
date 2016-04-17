20daa520cfd633f7a3288f8aa2473180489b5e73
Sherman Pay <shermanpay1991@gmail.com>
2016-04-17 00:20:29 -0700

    sendMix with func

--- public/scripts/send.js
+++ public/scripts/send.js
@@ -1,7 +1,9 @@
 define(function(require) {
 	var common = require("./common");
 	var mix = require("./mix");
-    document.getElementById("sendMixButton").onclick = mix.sendMix;
+    document.getElementById("sendMixButton").onclick = function() {
+		mix.sendMix();
+	};
 });
 
 
