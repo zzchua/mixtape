684e70dc34abdbd4689bfed584d8c091fcd5b04e
Zack Chua <zzchua@cs.washington.edu>
2016-04-16 21:25:15 -0700

    indentations

--- public/scripts/common.js
+++ public/scripts/common.js
@@ -1,14 +1,13 @@
-
 define(function() {
 	var ref = new Firebase("https://scorching-heat-6803.firebaseio.com");
-    var common = {
-        ref: ref,
-    	logout: (function logout() {
-				    console.log("loggingout");
-				    ref.unauth();
-				    alert("You are logged out!! going back into the login screen");
-				    window.location = "index.html";
+		var common = {
+				ref: ref,
+			logout: (function logout() {
+						console.log("loggingout");
+						ref.unauth();
+						alert("You are logged out!! going back into the login screen");
+						window.location = "index.html";
 				})
 	}
-    return common;
+	return common;
 });
