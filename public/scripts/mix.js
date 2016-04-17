156ed4fae8694d9d3436993a27cdf04525d1d071
kyaw <kyaw@uw.edu>
2016-04-16 23:14:03 -0700

    started implementing send action

--- public/scripts/mix.js
+++ public/scripts/mix.js
@@ -1,5 +1,19 @@
 define(function(require) {
-    function createMix(){
-    	val mix = {"album":"Sgt. Pepper's Lonely Hearts Club Band","artist":"The Beatles","comments":"","message":"EEEEEEK","reciever":1,"sender":2,"track":"With a Little Help From My Friends"} ;
+    var common = require('./common');
+    var ref = common.ref;
+    document.getElementById("sendMixButton").onclick = sendMix;
+
+    function sendMix(){
+    	var reciever = "facebook:10156785213465257";
+    	var sender = "facebook:10156785213465257";
+    	var songAlbum = document.getElementById("songAlbumInput").value;
+    	var artist = document.getElementById("artistInput").value;
+    	alert("sending mix " + artist + " " + songAlbum);
+    	var mixesRef = ref.child("mixes");
+    	var mix = {"recievers":{"reciever":reciever},"sender":sender, 
+    		"songAlbum":songAlbum, "artist":artist, "comments":{"comment":""}};
+    	mixesRef.push(mix);
+    	window.location = "feed.html";
     }
 });
+
