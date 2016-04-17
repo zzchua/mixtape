22f35dc5eeb7509a5269e4b86ac6bdf99f1dbd3d
Zack Chua <zzchua@cs.washington.edu>
2016-04-17 05:36:18 -0700

    adding username validation

--- public/scripts/username.js
+++ public/scripts/username.js
@@ -4,6 +4,20 @@ define(function(require) {
     var $=require("./jquery");
     $("#send-username").click(function() {
         var handle = $("#handle-input").val();
+        var regex = /^[a-zA-Z0-9-_]{5,12}$/;
+        if (handle.search(regex) == -1) {
+            alert("handle needs to be between 5 and 11 characters");
+            window.location = "username.html";
+        } else {
+            ref.child("users").on("value", function(snapshot) {
+                snapshot.forEach(function(data) {
+                    if (handle.toLowerCase() === data.handle.toLowerCase()) {
+                        alert("username has been taken");
+                        window.location = "username.html";
+                    }
+                });
+            });
+        }
         var uid = common.getCookies().uid;
         ref.child("users").child(uid).update(
             {
